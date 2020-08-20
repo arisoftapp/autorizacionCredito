a34feebfd75852a812c5034964cd9489eff15e54
Desarrollo <repositorios@arisoft.com.mx>
2020-08-20 11:49:52 -0600

    iniciando

--- src/AltaClientes.java
+++ src/AltaClientes.java
@@ -55,8 +55,14 @@ public class AltaClientes extends javax.swing.JFrame {
     private void initComponents() {
 
         jDialog1 = new javax.swing.JDialog();
-        btn_add = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        txt_codigo = new javax.swing.JTextField();
+        jPanel1 = new javax.swing.JPanel();
         add = new javax.swing.JLabel();
+        save = new javax.swing.JLabel();
+        btn_add = new javax.swing.JButton();
+        btn_guardar_cliente = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         txt_materno = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
@@ -68,93 +74,11 @@ public class AltaClientes extends javax.swing.JFrame {
         jLabel7 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         txt_comentarios = new javax.swing.JTextArea();
-        save = new javax.swing.JLabel();
-        btn_guardar_cliente = new javax.swing.JButton();
-        jPanel3 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        txt_codigo = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(700, 400));
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        btn_add.setText("jButton3");
-        btn_add.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_addActionPerformed(evt);
-            }
-        });
-        getContentPane().add(btn_add, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 270, 50, 50));
-
-        add.setText("jLabel2");
-        getContentPane().add(add, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 280, 30, 30));
-
-        jLabel3.setText("Nombre:");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 70, -1, -1));
-
-        txt_materno.setToolTipText("");
-        txt_materno.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txt_maternoKeyReleased(evt);
-            }
-        });
-        getContentPane().add(txt_materno, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 90, 180, -1));
-
-        jLabel4.setText("Apellido Paterno:");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 70, -1, -1));
-
-        jLabel5.setText("Apellido Materno:");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 70, -1, -1));
-
-        txt_nombre.setToolTipText("");
-        txt_nombre.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txt_nombreKeyReleased(evt);
-            }
-        });
-        getContentPane().add(txt_nombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 90, 230, -1));
-
-        txt_paterno.setToolTipText("");
-        txt_paterno.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txt_paternoKeyReleased(evt);
-            }
-        });
-        getContentPane().add(txt_paterno, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 90, 180, -1));
-
-        jLabel6.setText("Puesto:");
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, -1, -1));
-
-        txt_puesto.setToolTipText("");
-        txt_puesto.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txt_puestoKeyReleased(evt);
-            }
-        });
-        getContentPane().add(txt_puesto, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 140, 230, -1));
-
-        jLabel7.setText("Comentarios:");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 120, -1, -1));
-
-        txt_comentarios.setColumns(20);
-        txt_comentarios.setRows(5);
-        txt_comentarios.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                txt_comentariosKeyReleased(evt);
-            }
-        });
-        jScrollPane1.setViewportView(txt_comentarios);
-
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 140, 370, 110));
-        getContentPane().add(save, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 290, 30, 30));
-
-        btn_guardar_cliente.setText("Guardar");
-        btn_guardar_cliente.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_guardar_clienteActionPerformed(evt);
-            }
-        });
-        getContentPane().add(btn_guardar_cliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 400, 50, 50));
 
         jLabel1.setText("ID:");
         jPanel3.add(jLabel1);
@@ -171,7 +95,89 @@ public class AltaClientes extends javax.swing.JFrame {
         });
         jPanel3.add(txt_codigo);
 
-        getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 690, 40));
+        getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 700, 40));
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        add.setText("jLabel2");
+        jPanel1.add(add, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 210, 30, 30));
+        jPanel1.add(save, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 210, 30, 30));
+
+        btn_add.setText("jButton3");
+        btn_add.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_addActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn_add, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 200, 50, 50));
+
+        btn_guardar_cliente.setText("Guardar");
+        btn_guardar_cliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_guardar_clienteActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btn_guardar_cliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 200, 50, 50));
+
+        jLabel3.setText("Nombre:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 0, -1, -1));
+
+        txt_materno.setToolTipText("");
+        txt_materno.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txt_maternoKeyReleased(evt);
+            }
+        });
+        jPanel1.add(txt_materno, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 20, 180, -1));
+
+        jLabel4.setText("Apellido Paterno:");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 0, -1, -1));
+
+        jLabel5.setText("Apellido Materno:");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 0, -1, -1));
+
+        txt_nombre.setToolTipText("");
+        txt_nombre.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txt_nombreKeyReleased(evt);
+            }
+        });
+        jPanel1.add(txt_nombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 20, 230, -1));
+
+        txt_paterno.setToolTipText("");
+        txt_paterno.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txt_paternoKeyReleased(evt);
+            }
+        });
+        jPanel1.add(txt_paterno, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 20, 180, -1));
+
+        jLabel6.setText("Puesto:");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 50, -1, -1));
+
+        txt_puesto.setToolTipText("");
+        txt_puesto.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txt_puestoKeyReleased(evt);
+            }
+        });
+        jPanel1.add(txt_puesto, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 70, 230, -1));
+
+        jLabel7.setText("Comentarios:");
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 50, -1, -1));
+
+        txt_comentarios.setColumns(20);
+        txt_comentarios.setRows(5);
+        txt_comentarios.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                txt_comentariosKeyReleased(evt);
+            }
+        });
+        jScrollPane1.setViewportView(txt_comentarios);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 70, 370, 110));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 41, 700, 360));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -291,7 +297,7 @@ private int limite  = 10;
         }
          if(validar==true)
          {
-
+             guardar=true;
           desactivarCompClientes();
          }
          
@@ -304,7 +310,7 @@ private int limite  = 10;
           txt_puesto.setEnabled(false);
           txt_comentarios.setEnabled(false);
           txt_nombre.setEnabled(false);
-          btn_add.setEnabled(false);
+          //btn_add.setEnabled(false);
       }
           public JSONObject crearJsonInsert()
     {
@@ -331,49 +337,26 @@ private int limite  = 10;
     
     private void btn_addActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_addActionPerformed
         // TODO add your handling code here:
-
+        if(guardar==false)
+        {
+            cuadroDialogo("Falta Guardar Cliente");
+            RegistroHuella vista = null;
+            vista.param=txt_codigo.getText().toString().trim();
+            vista=new RegistroHuella();
+            vista.setVisible(true);
+        }
+        else
+        {
+            
+            RegistroHuella vista = null;
+            vista.param=txt_codigo.getText().toString().trim();
+            vista=new RegistroHuella();
+            vista.setVisible(true);
+        }
         
     }//GEN-LAST:event_btn_addActionPerformed
 
-    public void getfoto()
-    {
-        /*
-                FileNameExtensionFilter filtroImagen=new FileNameExtensionFilter("JPG, PNG & GIF","jpg","png","gif");
-        Object [] fila=new Object[2]; 
-        DefaultTableModel modelo=(DefaultTableModel) tabla_datos.getModel();
-        //Creamos el objeto JFileChooser
-        JFileChooser fc=new JFileChooser();
-         fc.setFileFilter(filtroImagen);
-        //Abrimos la ventana, guardamos la opcion seleccionada por el usuario
-        int seleccion=fc.showOpenDialog(this);
-        //Si el usuario, pincha en aceptar
-        if(seleccion==JFileChooser.APPROVE_OPTION){
-            //Seleccionamos el fichero
-            File fichero=fc.getSelectedFile();
-             //Ecribe la ruta del fichero seleccionado en el campo de texto
-            fila[0]=fichero.getAbsolutePath();
-            fila[1]=fichero.getName();
-            /*
-              try(FileReader fr=new FileReader(fichero)){
-              String cadena="";
-              int valor=fr.read();
-              while(valor!=-1){
-                cadena=cadena+(char)valor;
-                valor=fr.read();
-              }
-                  fila[1]=cadena;
-                  System.out.println(cadena);
-              } catch (IOException e1) {
-                e1.printStackTrace();
-              }
-            */
-              //modelo.addRow(fila);
-              //tabla_datos.setModel(modelo);
-              
-              
-        //}
-        
-    }
+
     private void txt_nombreKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txt_nombreKeyReleased
         // TODO add your handling code here:
         txt_nombre.setText(txt_nombre.getText().toUpperCase());
@@ -438,7 +421,7 @@ private int limite  = 10;
     }
     //variables
     consultasBD consultasBD=new consultasBD();
-
+    boolean guardar=false;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel add;
     private javax.swing.JButton btn_add;
@@ -450,6 +433,7 @@ private int limite  = 10;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel save;
