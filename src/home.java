ed13df69b28cc29fad6d607c4010adb4d4b229ef
Desarrollo <repositorios@arisoft.com.mx>
2020-08-25 10:45:33 -0600

    guardando imagen en servidor

--- src/home.java
+++ src/home.java
@@ -69,8 +69,6 @@ public class home extends javax.swing.JFrame {
         jButton3 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         btn_cerrarsesion = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -119,22 +117,6 @@ public class home extends javax.swing.JFrame {
             }
         });
         getContentPane().add(btn_cerrarsesion, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 160, 200, 100));
-
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 10, -1, -1));
-
-        jButton4.setText("jButton4");
-        jButton4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton4ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         jMenu1.setText("Clientes");
 
@@ -216,35 +198,6 @@ public class home extends javax.swing.JFrame {
         //this.setVisible(false);
   
     }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-     
-        try {
-            // TODO add your handling code here:
-            //Thread t = new home.atUploading();
-            //t.start();
-            prueba();
-        } catch (IOException ex) {
-            Logger.getLogger(home.class.getName()).log(Level.SEVERE, null, ex);
-        }
-   
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-        // TODO add your handling code here:
-               FileNameExtensionFilter filtroImagen=new FileNameExtensionFilter("JPG, PNG & GIF","jpg","png","gif");
-        JFileChooser fc=new JFileChooser();
-        fc.setFileFilter(filtroImagen);
-        //Abrimos la ventana, guardamos la opcion seleccionada por el usuario
-        int seleccion=fc.showOpenDialog(this);
-              //Si el usuario, pincha en aceptar
-        if(seleccion==JFileChooser.APPROVE_OPTION){
-            //Seleccionamos el fichero
-            fichero=fc.getSelectedFile();
-            //Ecribe la ruta del fichero seleccionado en el campo de texto  
-            //rsscalelabel.RSScaleLabel.setScaleLabel(lbl_foto, fichero.getAbsolutePath());
-        }
-    }//GEN-LAST:event_jButton4ActionPerformed
 public File fichero=null;
 
     /**
@@ -291,10 +244,8 @@ public File fichero=null;
     private javax.swing.JButton btn_cerrarsesion;
     private javax.swing.JLabel img_clientes;
     private javax.swing.JLabel img_logout;
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
